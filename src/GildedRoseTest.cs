697b916d857d34f1d7563f6836f4ae95f0e9d8d5
timmy.gilissen <timmy.gilissen@agidens.com>
2021-04-23 15:03:33 +0200

    gilded rose kata

--- src/GildedRoseTest.cs
+++ src/GildedRoseTest.cs
@@ -5,13 +5,40 @@ namespace csharpcore
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        
+        [Theory]
+        [InlineData("foo",0,10)]
+        [InlineData("Aged Brie",0,0)]
+        [InlineData("Elixir of the Mongoose",0,0)]
+        [InlineData("Sulfuras, Hand of Ragnaros",0,0)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert",0,0)]
+        public void Quality_can_never_decrease_below_0(string name, int sellIn, int quality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            var item = new Item { Name = name, SellIn = sellIn, Quality = quality } ;
+            // GildedRose.UpdateQuality(item);
+            Assert.True(item.Quality >= 0);
+        }
+        
+        [Theory]
+        [InlineData("foo", 5, 10, 9)]
+        [InlineData("foo", 5, 9, 8)]
+        [InlineData("foo", 0, 10, 8)]
+        public void Quality_should_decrease(string name, int sellIn, int quality, int expectedQuality)
+        {
+            var item = new Item { Name = name, SellIn = sellIn, Quality = quality } ;
+            // GildedRose.UpdateQuality(item);
+            Assert.Equal(expectedQuality, item.Quality);
+        }
+
+        [Theory]
+        [InlineData(5, 10, 11)]
+        [InlineData(4, 11, 12)]
+        [InlineData(3, 12, 13)]
+        public void Quality_of_agedbrie_increases_the_older_it_gets(int sellIn, int quality, int expectedQuality)
+        {
+            var item = new Item { Name = "Aged Brie", SellIn = sellIn, Quality = quality } ;
+            // GildedRose.UpdateQuality(item);
+            Assert.Equal(expectedQuality, item.Quality);
         }
     }
 }
--- src/Item.cs
+++ src/Item.cs
@@ -1,9 +1,110 @@
-﻿namespace csharpcore
+﻿using System;
+
+namespace csharpcore
 {
     public class Item
     {
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+    }
+
+    public class BackStagePass : Item, IItem
+    {
+        public void UpdateQuantity()
+        {
+            SellIn -= 1;
+
+            this.IncreaseQuality();
+
+            if (SellIn < 10)
+            {
+                this.IncreaseQuality();
+            }
+
+            if (SellIn < 5)
+            {
+                this.IncreaseQuality();
+            }
+
+            if (SellIn < 0)
+            {
+                Quality = 0;
+            }
+        }
+    }
+
+    public class Sulfuras : Item, IItem
+    {
+        public void UpdateQuantity()
+        {
+            return;
+        }
+    }
+
+    public class RegularItem : Item, IItem
+    {
+        public void UpdateQuantity()
+        {
+            this.DecreaseQuality();
+            
+            SellIn -= 1;
+
+            if (SellIn < 0) this.DecreaseQuality();
+        }
+    }
+    
+    public class AgedBrie : Item, IItem
+    {
+        public void UpdateQuantity()
+        {
+            SellIn -= 1;
+
+            this.IncreaseQuality();
+
+            if (SellIn >= 0) return;
+
+            this.IncreaseQuality();
+        }
+    }
+
+    public interface IItem
+    {
+        void UpdateQuantity();
+    }
+
+    public static class ItemExtentions
+    {
+        public static bool IsAgedBrie(this Item item)
+        {
+            return item.Name.Equals("Aged Brie", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public static bool IsSulfuras(this Item item)
+        {
+            return item.Name.Equals("Sulfuras, Hand of Ragnaros", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public static bool IsBackStagePass(this Item item)
+        {
+            return item.Name.Equals("Backstage passes to a TAFKAL80ETC concert",
+                StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public static void IncreaseQuality(this Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+            }
+        }
+
+        public static void DecreaseQuality(this Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality--;
+            }
+        }
     }
 }
